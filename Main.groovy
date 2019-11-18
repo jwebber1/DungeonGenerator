b4622ccf6c3c1d9db75535f05602d03836f780b2
Jon <34067696+jwebber1@users.noreply.github.com>
2019-11-18 07:36:39 -0800

    Dungeon Generator v0.2
    Decide/create list/arraylist global
    For loops to call function(list, i, j)
    Function
    Set random legend thing
    Print that out to the console
    Optional: continue to contemplate logic for the algorithm

--- Main.groovy
+++ Main.groovy
@@ -5,7 +5,6 @@ Sprint 1 details:
         a) If non-validated, give another chance
     3) Print something out to the console
 */
-
 println("Please enter the desired size of your dungeon:")
 
 def width = 0, height = 0, monsters = -1
@@ -20,5 +19,42 @@ while(height < 1){
     try { height = System.in.newReader().readLine() as Integer }
     catch (NumberFormatException err){ println("It appears you did not put in an integer greater than 0. Please try again.") }
 }
+while(monsters < 0){
+    print("Number of Monsters: ")
+    try { monsters = System.in.newReader().readLine() as Integer }
+    catch (NumberFormatException err){ println("It appears you did not put in 0 or an integer greater than 0. Please try again.") }
+}
 
-print 'Dungeon: ' + width + ' by ' + height
+//--------------------new -----------------------------
+
+List environment = []
+
+fillEnvironment(environment, width, height)
+printEnvironment(environment)
+
+def printEnvironment(environment){
+    for (row in environment){
+        for(item in row){ printf('%s  ',item) }
+        println()
+    }
+}
+
+def fillEnvironment(environment, row, col){
+    rand = new Random()
+
+    for(int i = 0; i < col; i++){
+        environment.add([])
+
+        for(int j = 0; j < row; j++){
+            r = rand.nextInt(100)
+            c = ''
+
+            if(r < 50){ c = 'P' }
+            else if(r < 90){ c = 'W' }
+            else{ c = 'T' }
+
+            environment[i][j] = c
+        }
+    }
+}
+//--------------end of new------------
